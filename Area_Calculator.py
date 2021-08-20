8b4386c93a47047cb09aee28919c72f517ccd312
Nchia-Emmanuela <83321016+Nchia-Emmanuela@users.noreply.github.com>
2021-08-20 10:05:34 +0100

    function creation

---
+++ Area_Calculator.py
@@ -0,0 +1,8 @@
+def paint_cal(test_h, test_w, coverage, cans):
+    test_h = int(input("Height of wall: \n"))
+    test_w = int(input("Width of wall: \n"))
+    coverage = 5
+    cans = test_h*
+paint_cal(height=test_h, width = test_w, cover = coverage, )
+
+
