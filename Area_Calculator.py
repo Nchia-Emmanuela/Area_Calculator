1c69bb68e066697bd97c93b90fa4725a2a8a2ae4
Nchia-Emmanuela <83321016+Nchia-Emmanuela@users.noreply.github.com>
2021-08-20 10:18:06 +0100

    getting user Input

--- Area_Calculator.py
+++ Area_Calculator.py
@@ -3,3 +3,7 @@ import math
 def paint_calc(height, width, cover):
     area = height * width
     num_of_cans = math.ceil(area / cover)
+    print(f"you'll need {num_of_cans} cans of paint.")
+
+test_h = int(input("Height of Wall: \n "))
+test_w = int(input("Width of Wall: \n"))
